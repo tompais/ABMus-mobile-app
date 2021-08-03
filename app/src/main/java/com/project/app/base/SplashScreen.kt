ae74c8022cd5c81ffdfc394d093bb22d1a38a6e2
alejandracasuscelli <acasuscelli@globons.com>
2021-08-03 16:07:06 -0300

    base project

---
+++ app/src/main/java/com/project/app/base/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.project.app.base
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.project.app.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
